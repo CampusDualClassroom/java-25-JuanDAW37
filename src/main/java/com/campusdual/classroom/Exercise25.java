177b8df6505127ed3680fe88a0c1d3827980eb0c
JuanDAW37 <jymtr1968@gmail.com>
2024-10-03 12:53:33 +0200

    Exercicio 25 listo

--- src/main/java/com/campusdual/classroom/Exercise25.java
+++ src/main/java/com/campusdual/classroom/Exercise25.java
@@ -1,20 +1,35 @@
 package com.campusdual.classroom;
 
+import java.util.Iterator;
 import java.util.Stack;
 
 public class Exercise25 {
-    public static Stack<String> createStack() {
 
+    public static Stack<String> pila = new Stack<>();
+
+    public static Stack<String> createStack() {
+        Stack<String> auxPila = new Stack<>();
+
+        pila.push("Smith");
+        pila.push("Montessori");
+        pila.push("Peralta");
+        pila.push("House");
+
+        for (int i = pila.size(); i > 0; i--) {
+            System.out.println(pila.get(i-1));
+        }
+
+        return pila;
     }
 
     public static void printAndEmptyStack(Stack<String> stack) {
-
+        while(!stack.isEmpty()){
+            System.out.println(stack.pop());
+        }
     }
 
     public static void main(String[] args) {
-
+        pila = createStack();
+        printAndEmptyStack(pila);
     }
-
-
-
 }
